7f7428d61893eb0fea0427a0a725bb34dc21beb7
tomlintontx <tomlintontx@gmail.com>
2020-09-21 15:09:54 -0600

    Add files via upload

---
+++ ProgressMeter/myWidget.js
@@ -0,0 +1,107 @@
+function putMeOnTheScreen(widget, event, config) {
+    var ele = config.ele;
+    var width = ele.width();
+    var height = ele.height();
+    var myDiv = ele[0];
+    const tagName = "tomtom-123456";
+    myDiv.setAttribute("style","width: 99%; height: 99%; margin: 0 auto");
+    myDiv.setAttribute("id",tagName);
+    var myGoal = widget.queryResult.$$rows[0][1].text;
+
+    function radialProgress(selector) {
+        const parent = d3.select(selector)
+        const size = parent.node().getBoundingClientRect()
+        const svg = parent.append('svg')
+          .attr('width', size.width)
+          .attr('height', size.height);
+        const outerRadius = Math.min(size.width, size.height) * 0.45;
+        const thickness = 10;
+        let value = 0;
+        
+        const mainArc = d3.arc()
+          .startAngle(0)
+          .endAngle(Math.PI * 2)
+          .innerRadius(outerRadius-thickness)
+          .outerRadius(outerRadius)
+      
+        svg.append("path")
+          .attr('class', 'progress-bar-bg')
+          .attr('transform', `translate(${size.width/2},${size.height/2})`)
+          .attr('d', mainArc())
+        
+        const mainArcPath = svg.append("path")
+          .attr('class', 'progress-bar')
+          .attr('transform', `translate(${size.width/2},${size.height/2})`)
+          .attr('fill', config.color)
+        
+        svg.append("circle")
+          .attr('class', 'progress-bar')
+          .attr('transform', `translate(${size.width/2},${size.height/2-outerRadius+thickness/2})`)
+          .attr('width', thickness)
+          .attr('height', thickness)
+          .attr('r', thickness/2)
+          .attr('fill', config.color)
+      
+        const end = svg.append("circle")
+          .attr('class', 'progress-bar')
+          .attr('transform', `translate(${size.width/2},${size.height/2-outerRadius+thickness/2})`)
+          .attr('width', thickness)
+          .attr('height', thickness)
+          .attr('r', thickness/2)
+          .attr('fill', config.color)
+        
+        let percentLabel = svg.append("text")
+          .attr('class', 'progress-label')
+          .attr('transform', `translate(${size.width/2},${size.height/2 - 5})`)
+          .text('0')
+
+        let goalLabel = svg.append("text")
+          .attr('class', 'goal-label')
+          .attr('transform', `translate(${size.width/2},${size.height/2 + 15})`)
+          .text('0')
+      
+        return {
+            update: function(progressPercent) {
+                const startValue = value
+                const startAngle = Math.PI * startValue / 50
+                const angleDiff = Math.PI * progressPercent / 50 - startAngle;
+                const startAngleDeg = startAngle / Math.PI * 180
+                const angleDiffDeg = angleDiff / Math.PI * 180
+                const transitionDuration = 1500
+        
+                mainArcPath.transition().duration(transitionDuration).attrTween('d', function(){
+                return function(t) {
+                    mainArc.endAngle(startAngle + angleDiff * t)
+                    return mainArc();
+                }
+                })
+                end.transition().duration(transitionDuration).attrTween('transform', function(){
+                return function(t) {
+                    return `translate(${size.width/2},${size.height/2})`+
+                    `rotate(${(startAngleDeg + angleDiffDeg * t)})`+
+                    `translate(0,-${outerRadius-thickness/2})`
+                }
+                })
+                percentLabel.transition().duration(transitionDuration).tween('bla', function() {
+                return function(t) {
+                    percentLabel.text(widget.queryResult.$$rows[0][0].text)
+                }
+                })
+                goalLabel.transition().duration(transitionDuration).tween('bla', function() {
+                    return function(t) {
+                        goalLabel.text('Goal: ' + myGoal)
+                    }
+                    })
+                value = progressPercent
+
+            }
+        }
+    }
+      
+    let chart = radialProgress('#' + tagName)
+    let widgetResults = Math.round((widget.queryResult.$$rows[0][0].data/widget.queryResult.$$rows[0][1].data) * 100)
+    chart.update(widgetResults)
+    
+}
+
+
---
+++ ProgressMeter/widget.js
@@ -0,0 +1,161 @@
+prism.registerWidget("ProgressMeter", {
+    name: "ProgressMeter",
+    family: "Indicator",
+    title: "Progress Meter",
+    iconSmall: "/plugins/ProgressMeter/ProgressMeter-icon-small.png",
+    styleEditorTemplate: "/plugins/ProgressMeter/styler.html",
+    hideNoResults: true,
+    directive: {
+        desktop: "ProgressMeter"
+    },
+    style: {
+        /* object structure for styling use, available to be changed
+        using the design panel*/
+        color: "black"
+    },
+    data: {
+        selection: [],
+        defaultQueryResult: {},
+        panels: [
+            {
+                name: "Metric",
+                itemAttributes: ["color"],
+				allowedColoringTypes: function () {
+					return {
+						color: true,
+						condition: true,
+						range: false
+					};
+				},
+				itemAdded(widget, item) {
+                    // Adding a new color
+                    if (typeof item.format === 'undefined') {
+                        // Grab first color from palette
+                        const firstColor = prism.activeDashboard.style.palette()[0];
+                        // Create a format obj
+                        item.format = {
+                            color: {
+                                type: 'color',
+                                color: firstColor,
+                            },
+                            mask: {
+                                type: 'number',
+                                abbreviations: {
+                                    t: true,
+                                    b: true,
+                                    m: true,
+                                    k: true,
+                                },
+                                separated: true,
+                                decimals: 'auto',
+                                isdefault: true,
+                            },
+                        };
+                    }
+                    // Return the item
+                    return item;
+                },
+                metadata: {
+                    types: ['measures'],
+                    maxitems: 1,
+                },
+                visibility: true,
+            },
+            {
+                name: "Goal",
+                metadata: {
+                    types: ['measures'],
+                    maxitems: 1,
+                },
+                visibility: true,
+            },
+            {
+                name: "filters",
+                type: "filters",
+                metadata: {
+                    types: ["dimensions"],
+                    maxitems: -1
+                },
+            },
+        ],
+        canColor: function (widget, panel, item) {
+			if (panel.name == "Metric") {
+				return true;
+			} else {
+				return false;
+			}
+		},
+        // builds a jaql query from the given widget
+        buildQuery: function (widget) {
+
+            if (widget.metadata.panel("Metric").items.length === 0 || widget.metadata.panel("Goal").items.length === 0) {
+                var query = {
+                    datasource: widget.datasource,
+                    format: "json",
+                    isMaskedResult: true,
+                    metadata: []
+                };
+
+                return query;
+
+            } else {
+                // building jaql query object from widget metadata
+                var query = {
+                    datasource: widget.datasource,
+                    format: "json",
+                    isMaskedResult: true,
+                    metadata: []
+                };
+
+                // Metric Attr
+                var curPanel = widget.metadata.panel("Metric").items[0];
+                query.metadata.push(curPanel);
+
+                //Target Attr
+                var targetMetric = widget.metadata.panel("Goal").items[0];
+                query.metadata.push(targetMetric)
+
+                // gather the filters, don't need to change
+                if (defined(widget.metadata.panel("filters"), "items.0")) {
+                    widget.metadata.panel("filters").items.forEach(function (item) {
+                        item = $$.object.clone(item, true);
+                        item.panel = "scope";
+                        query.metadata.push(item);
+                    });
+                }
+
+                return query;
+            }
+        },
+        processResult: function (widget, queryResult) {
+            
+            return queryResult
+
+        },
+    },
+    /**
+     * Get all the connected dimensions selected filters
+     * @param {*} widget
+     * @param {*} event
+     */
+    beforequery(widget, event) {
+    },
+    render: function (widget, event) {
+        var ele = $(event.element);
+        ele.empty();
+
+        var config = {
+            color: widget.queryResult.$$rows[0][0].color,
+            ele: ele,
+            maxNum: widget.queryResult.$$rows[0][1].data
+        }
+
+        if(widget.queryResult.$$rows.length > 0) {
+            putMeOnTheScreen(widget, event, config);
+        }
+
+    },
+    options: {
+        dashboardFiltersMode: "slice",
+    },
+});
